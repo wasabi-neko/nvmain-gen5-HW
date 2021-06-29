b91c34e813cd7d90af8ca25c69732ff03763d466
wasabi-neko <wasabi.neko.owo@gmail.com>
2021-06-29 14:35:45 +0800

    Rerun Q4-write-through

--- project/result/q4/mul-wb/cmd.sh
+++ project/result/q4/mul-wb/cmd.sh
@@ -9,6 +9,7 @@ mul=/home/user/project/benchmark/multiply.out
 arg="
     --cpu-type=TimingSimpleCPU
     --caches --l2cache --l3cache
+    --l3_assoc=4
     --l1i_size=32kB --l1d_size=32kB --l2_size=128kB --l3_size=1MB
     --mem-type=NVMainMemory
     --nvmain-config=/home/user/project/NVmain/Config/PCM_ISSCC_2012_4GB.config
